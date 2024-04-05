56c3e260b4d8874ea9a4241f3657a7919cd64019
Chathuranga Sampath <csampath.work@gmail.com>
2024-04-05 21:50:20 +0530

    fix: Adjust packet extraction frame limit for special case
    This change modifies the hex extraction process to automatically adjust the frame limit to accommodate a special case where 25 packets are required instead of the previous 24.
    
    Changes Made:
    
    Updated the packet extraction process to adjust frame limit for special case where more packets are required
    Ensured the hex extraction process adapts to varying packet requirements

--- hex-extractor.py
+++ hex-extractor.py
@@ -1,12 +1,13 @@
 import os
 import json
+from constants.hex_constants import ControlDataPoint, SpecialDataPoint
 
 input_json_file_path = r"hex\raw-data\sample.json"
-
-init_comm = '04ab0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
-end_comm = '04f00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
+isStaticEffect = False
+frameThreashold = 24
 
 def extract_data_from_packets(packets):
+    global frameThreashold
     # Extracting 'data_fragment' from each dictionary and creating a list
     extracted_data = []
 
@@ -17,16 +18,23 @@ def extract_data_from_packets(packets):
         data_fragment = packet['_source']['layers']['Setup Data']['usb.data_fragment']
         formatted_data = data_fragment.replace(':', '')
 
+        print(formatted_data)
+
         # Start extraction when specific data is encountered
-        if formatted_data == init_comm:
+        if formatted_data == ControlDataPoint.INIT_COMM:
             start_extraction = True
         
         # Append data to the list if extraction flag is True
         if start_extraction:
             extracted_data.append(formatted_data)
+            
+            found = any(formatted_data == getattr(SpecialDataPoint, attr) for attr in dir(SpecialDataPoint) if not attr.startswith('__'))
+            if found:
+                print("Special data Point detected")
+                frameThreashold += 1
 
         # Stop extraction when specific data is encountered
-        if formatted_data == end_comm:
+        if formatted_data == ControlDataPoint.END_COMM:
             start_extraction = False
     
     return extracted_data
@@ -34,9 +42,9 @@ def extract_data_from_packets(packets):
 def update_or_create_output_json(input_json_file_path, extracted_data):
     # Extract the filename from the provided input file path
     filename = os.path.splitext(os.path.basename(input_json_file_path))[0]
-
+    print("Extracting ",filename)
     # Define the output JSON file path
-    output_json_file_path = "hex\data.json"
+    output_json_file_path = "data.json"
 
     # Create the data structure if it doesn't exist
     if not os.path.exists(output_json_file_path):
@@ -71,7 +79,7 @@ extracted_data = extract_data_from_packets(packets)
 preCount = len(packets)
 postCount = len(extracted_data)
 
-if(postCount == 24):
+if(postCount == frameThreashold):
     print('Extraction successful')
     print("pre-extractions frame count: ", preCount)
     print("post-extractions frame count: ", postCount)
